848acc5225e8c1e84680ca7fed8a699235214e3b
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-09-11 04:02:18 +0500

    cookie

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -49,7 +49,7 @@ async function signin(req, res) {
           sameSite: "none",
           secure: true,
           domain: "lions-webapp.vercel.app",
-          path: "/login",
+          path: "/",
         };
         // const prodOptions = {};
         console.log({ userId: preUser._id, token });
