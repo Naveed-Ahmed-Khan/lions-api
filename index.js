6d82d62bca657595b0756052acfd24a5d35fecce
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-09-12 01:08:35 +0500

    allowed headers

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: true,
   credentials: true,
+  allowedHeaders: ["content-type", "Authorization", "set-cookie"],
   exposedHeaders: ["set-cookie"],
 };
 
