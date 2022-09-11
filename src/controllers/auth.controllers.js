201af97c8ddda42c88027be1fbeb987711c015b4
Naveed-Ahmed-Khan <naveedahmedkhan12234@gmail.com>
2022-09-11 05:09:03 +0500

    cookie

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -54,7 +54,8 @@ async function signin(req, res) {
         // const prodOptions = {};
         console.log({ userId: preUser._id, token });
         res.cookie("token", token, prodOptions);
-        // res.cookie("user_id", preUser._id.toString(), prodOptions);
+        res.cookie("user_id", preUser._id.toString(), prodOptions);
+
         res.status(200).json(preUser);
         /* res.status(200).json({
           userId: preUser._id,
